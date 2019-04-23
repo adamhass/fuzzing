e94d397d33b814d45f4c6e1cd654e7423ac0d7b1
Adam Hasselberg <adamhasselberg@gmail.com>
2019-04-23 10:37:33 +0200

    First submission files created, not minimized, not UTF-8

--- createTestCases.sh
+++ createTestCases.sh
@@ -8,31 +8,10 @@ for dname in output/*/; do
 	# Create txt files
 	for filename in ./output/${dname}/*; do
 		[ -e "$filename" ] || continue
-		cat $filename > ./tools/bin_to_string > $filename.temp.txt
+		cat $filename | ./tools/bin_to_string >> $filename.temp.txt
 	    done
 	# Create the submission file
-	cat ./output/${dname}/*.temp.txt > ./submission/${dname}.txt
+	cat ./output/${dname}/*.temp.txt >> ./submission/${dname}.txt
 	# Clear out the temporary
 	rm ./output/${dname}/*.temp.txt
 	done
-
-# cat ./output/cat/* > ./submission/cat.txt
-# cat ./output/date/* > ./submission/date.txt
-# cat ./output/df/* > ./submission/df.txt
-# cat ./output/echo/* > ./submission/echo.txt
-# cat ./output/ln/* > ./submission/ln.txt
-# cat ./output/mkdir/* > ./submission/mkdir.txt
-# cat ./output/mv/* > ./submission/mv.txt
-# cat ./output/pwd/* > ./submission/pwd.txt
-# cat ./output/touch/* > ./submission/touch.txt
-# cat ./output/uname/* > ./submission/uname.txt
-# cat ./output/cp/* > ./submission/cp.txt
-# cat ./output/dd/* > ./submission/dd.txt
-# cat ./output/dir/* > ./submission/dir.txt
-# cat ./output/false/* > ./submission/false.txt
-# cat ./output/ls/* > ./submission/ls.txt
-# cat ./output/mktemp/* > ./submission/mktemp.txt
-# cat ./output/printf/* > ./submission/printf.txt
-# cat ./output/sleep/* > ./submission/sleep.txt
-# cat ./output/true/* > ./submission/true.txt
-# cat ./output/vdir/* > ./submission/vdir.txt
